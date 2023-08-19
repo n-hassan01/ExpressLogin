7163e43b51f780223add69c67391c9190bb07bca
n-hassan01 <naimul.hassan.se@gmail.com>
2023-08-19 12:17:22 +0600

    login service using express

---
+++ index.js
@@ -0,0 +1,28 @@
+/* eslint-disable prettier/prettier */
+const express = require('express');
+const cors = require('cors');
+const loginRoute = require('./controllers/loginService');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// routing middleware
+app.use('/', loginRoute);
+
+// error handling middleware
+app.use((req, res, next) => {
+    next('Requested url not found!');
+});
+
+app.use((err, req, res) => {
+    if (err.message) {
+        res.status(500).send(err.message);
+    } else {
+        res.status(500).send('Server side error!');
+    }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
